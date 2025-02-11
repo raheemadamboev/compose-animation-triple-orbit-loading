8923fc22de61b2f1dc417fa17809255ebdda1ee4
raheem <raheemideadev@gmail.com>
2025-02-12 01:46:00 +0500

    Setup Material3;

--- app/src/main/java/xyz/teamgravity/composeanimationtripleorbitloading/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/composeanimationtripleorbitloading/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { padding ->
+
                 }
             }
         }
